6848b9f68bdff1aa68c96c0584005514fd1d2324
zachvance <zachariah.vance@live.com>
2021-05-26 11:24:24 -0400

    Updates

---
+++ config.py
@@ -0,0 +1,8 @@
+
+DRIVER = r"C:\Users\ZVance\Downloads\chromedriver_win32\chromedriver.exe"
+
+SITE = "https://dash.sctc.rideco.com/#/login"
+
+USERNAME = "username"
+
+PASSWORD = "password"
--- main.py
+++ main.py
@@ -1,19 +1,13 @@
-import requests
-response = requests.get("https://dash.sctc.rideco.com/#/exports",
-                    params = {'agendas':'7c336a5f-4224-48a2-91a7-bddb36e2954f',
-                              'export_type': 'ride',
-                              'first_date':'2021-01-06',
-                              'programs':'a0b63f37-a251-4e15-a769-36432ad8c00d',
-                              'second_date':'2021-01-06', # YYYY-MM-DD format
-                              'timezone':'America/Toronto'})
-print(response.url)
-print(response.text)
-print(response.headers)
 
-'''
-response = requests.post('https://localhost:4000/bananas/12345.png', data = '[ 1, 2, 3, 4 ]')
-data = response.content
+from config import DRIVER, SITE, USERNAME, PASSWORD
+from selenium import webdriver
 
-with open(path, 'wb') as s:
-    s.write(data)
-'''
+driver = webdriver.Chrome(DRIVER)
+driver.get(SITE)
+assert "rideco" in driver.title
+
+def site_login():
+    driver.get(site)
+    driver.find_element_by_id(username).send_keys(USERNAME)
+    driver.find_element_by_id(password).send_keys(PASSWORD)
+    driver.find_element_by_id(btn-login).click()
