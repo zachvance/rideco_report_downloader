591543849ec576c14ff0cdcb8c2f3846fc186027
zachvance <77811876+zachvance@users.noreply.github.com>
2021-11-29 14:50:28 -0500

    Refactoring

--- config.py
+++ config.py
@@ -4,13 +4,12 @@ USERNAME = "email"
 
 PASSWORD = "password"
 
-FIRST_DATE = '2021-10-01'
+FIRST_DATE = "2021-10-01"
 
-SECOND_DATE = '2021-10-03'
+SECOND_DATE = "2021-10-03"
 
-EXPORT_TYPES = ['ride', 'fare', 'vehicle_hours']
+EXPORT_TYPES = ["ride", "fare", "vehicle_hours"]
 
-program_001 = '268298b4-73bb-415c-8fb5-6a5524ccceb6'  # St. Catharines program code
-program_002 = 'a0b63f37-a251-4e15-a769-36432ad8c00d'  # Thorold program code
+program_001 = "268298b4-73bb-415c-8fb5-6a5524ccceb6"  # St. Catharines program code
+program_002 = "a0b63f37-a251-4e15-a769-36432ad8c00d"  # Thorold program code
 PROGRAMS = [program_001, program_002]
-
--- requests_based.py
+++ requests_based.py
@@ -7,54 +7,60 @@ own program codes from the RideCo site to set up this script.
     -   Stitch downloaded reports together into a single file (per export_type)
 """
 
-import pandas as pd
-import requests
 import csv
 from pathlib import Path
-from config import EXPORT_TYPES, FIRST_DATE, SECOND_DATE, PROGRAMS
+
+import pandas as pd
+import requests
+
+from config import EXPORT_TYPES, FIRST_DATE, PROGRAMS, SECOND_DATE
 
 cwd = Path.cwd()
-path = str(cwd) + '\\files'
+path = str(cwd) + "\\files"
 Path(path).mkdir(parents=True, exist_ok=True)
 
-date_list = [d.strftime('%Y-%m-%d') for d in pd.date_range(FIRST_DATE, SECOND_DATE)]
+date_list = [d.strftime("%Y-%m-%d") for d in pd.date_range(FIRST_DATE, SECOND_DATE)]
 
 for date in date_list:
     for export_type in EXPORT_TYPES:
         for program in PROGRAMS:
-            path = '/dash/rest/exports?' \
-                   'export_type=' + export_type + \
-                   '&first_date=' + date + \
-                   '&programs=' + program + \
-                   '&second_date=' + date + \
-                   '&timezone=America/Toronto'
-
-            headers = {'authority': 'sctc.rideco.com',
-                       'path': path,
-                       'accept': 'application/json; version=dash-0.43.1',
-                       'authorization': 'Token 0e78db1173d17ebad26a5472b9393031d11abf0b',
-                       }
-
-            url = 'https://sctc.rideco.com' + path
-
-            file_name = date + '-' + \
-                        date + '-' + \
-                        export_type + '-' + \
-                        program + \
-                        '.csv'
-
-            r = requests.get(
-              url=url,
-              headers=headers,
+            path = (
+                "/dash/rest/exports?"
+                "export_type="
+                + export_type
+                + "&first_date="
+                + date
+                + "&programs="
+                + program
+                + "&second_date="
+                + date
+                + "&timezone=America/Toronto"
             )
 
-            with open('files\\temp.csv', 'w') as file:
-              file.write(r.text)
+            headers = {
+                "authority": "sctc.rideco.com",
+                "path": path,
+                "accept": "application/json; version=dash-0.43.1",
+                "authorization": "Token 0e78db1173d17ebad26a5472b9393031d11abf0b",
+            }
 
-            in_fnam = 'files\\temp.csv'
-            out_fnam = 'files\\' + file_name
+            url = "https://sctc.rideco.com" + path
 
-            with open(in_fnam) as input, open(out_fnam, 'w', newline='') as output:
+            file_name = date + "-" + date + "-" + export_type + "-" + program + ".csv"
+
+            r = requests.get(
+                url=url,
+                headers=headers,
+            )
+
+            # Write the initial file.
+            with open("files\\temp.csv", "w") as file:
+                file.write(r.text)
+
+            # Remove the blank lines from the CSV.
+            in_file = "files\\temp.csv"
+            out_file = "files\\" + file_name
+            with open(in_file) as input, open(out_file, "w", newline="") as output:
                 writer = csv.writer(output)
                 for row in csv.reader(input):
                     if any(field.strip() for field in row):
